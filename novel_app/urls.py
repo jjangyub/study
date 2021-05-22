d6b6d9b3ab1e14622fbf3a564773c46851aa7d56
jjangyub <jjangyub@naver.com>
2021-05-23 01:08:15 +0900

    study_1commit

---
+++ novel_app/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+        path('<str:character1>/<str:character2>/',views.my_novel,name='my_novel_haha')]
---
+++ novel_app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import redirect,render
+def my_novel(request, character1, character2):
+    context = {"char_1": character1, "char_2":character2}
+    return render(request,'novel_html.html', context)
+# Create your views here.
